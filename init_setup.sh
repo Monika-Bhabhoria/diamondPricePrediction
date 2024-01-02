1d65b3a562388e94eb8cfbabeeaa0bac44a9c4e8
Monika Bhabhoria <ms.monika5592@gmail.com>
2024-01-02 21:55:00 +0530

    shell script added

---
+++ init_setup.sh
@@ -0,0 +1,15 @@
+echo [$(date)]: "START"
+
+echo [$(date)]: "creating env"
+
+conda create --prefix ./env python==3.8 -y
+
+echo [$(date)]: "Activating Env"
+
+source activate ./env
+
+echo [$(date)]: "Installing the requirements.txt"
+
+pip install -r requirements.txt
+
+echo [$(date)]: "END"
